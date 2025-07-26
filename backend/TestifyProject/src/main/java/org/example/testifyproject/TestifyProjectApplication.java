7835682c0636af1a32d5a37ee6a34013ae7a0238
SECRET-PC\phuca <phucanphan1995@gmail.com>
2025-07-26 15:01:34 +0700

    Init back-end project

---
+++ backend/TestifyProject/src/main/java/org/example/testifyproject/TestifyProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.testifyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestifyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestifyProjectApplication.class, args);
+	}
+
+}
---
+++ backend/TestifyProject/src/test/java/org/example/testifyproject/TestifyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.testifyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestifyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
